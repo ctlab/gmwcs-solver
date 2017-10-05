f13ed87d4c376d299b43fb9c109191ff0184250b
Nikolai <n.poperechnij@gmail.com>
2017-10-05 22:34:36 +0300

    dijkstra done

--- src/main/java/ru/ifmo/ctddev/gmwcs/solver/Dijkstra.kt
+++ src/main/java/ru/ifmo/ctddev/gmwcs/solver/Dijkstra.kt
@@ -10,7 +10,8 @@ import java.util.*
 /**
  * Created by Nikolay Poperechnyi on 04/10/2017.
  */
-class Dijkstra(val graph: Graph) {
+class Dijkstra(val graph: Graph, val from: Node) {
+    private val s = from.num;
 
     private val n = graph.vertexSet().size
 
@@ -21,20 +22,19 @@ class Dijkstra(val graph: Graph) {
             { DoubleArray(n, { Double.MAX_VALUE }) }
     )
 
-
-    private fun solve(from: Node): Unit {
-        val s = from.num
+    private fun solve(): Unit {
         val queue = PriorityQueue<Node>(
                 { n1, n2 -> (d[s][n1.num] - d[s][n2.num]).compareTo(0) }
         )
         d[s][s] = 0.0
-        visited[s] = true
         queue.add(from)
         while (queue.isNotEmpty()) {
             val cur = queue.peek()
             visited[cur.num] = true
             for (adj in graph.neighborListOf(cur).filter { !visited[it.num] }) {
+                // 0 for positive, -weight for negative
                 val w = d[s][cur.num] - minOf(graph.getEdge(cur, adj).weight, 0.0)
+                                      - minOf(cur.weight, 0.0)
                 if (d[s][adj.num] > w) {
                     d[s][adj.num] = w
                     queue.add(adj)
@@ -43,14 +43,12 @@ class Dijkstra(val graph: Graph) {
         }
     }
 
-
-    fun negativeEdges(s: Node): List<Edge> {
-        if (d[s.num][s.num] == Double.MAX_VALUE)
-            solve(s)
-        return graph.edgesOf(s)
-                .map { Pair(it, graph.getOppositeVertex(s, it)) }
-                .filter { it.first.weight < 0 && d[s.num][it.second.num] <= it.first.weight }
-                .unzip().first
+    fun negativeEdges(): List<Edge> {
+        if (d[s][s] == Double.MAX_VALUE)
+            solve()
+        return graph.edgesOf(from).filter { it.weight <= 0
+                    && d[s][graph.getOppositeVertex(from, it).num] < -it.weight
+        }
     }
 
 
--- src/main/java/ru/ifmo/ctddev/gmwcs/solver/Preprocessor.kt
+++ src/main/java/ru/ifmo/ctddev/gmwcs/solver/Preprocessor.kt
@@ -11,36 +11,55 @@ class Preprocessor(val graph: Graph) {
 
     fun Graph.getAdjacent(e: Edge) = Pair(this.getEdgeSource(e), this.getEdgeTarget(e))
 
-
     fun preprocess(): Unit {
-        for (edge in graph.edgeSet().toList()) {
-            if (!graph.containsEdge(edge)) {
-                continue;
-            }
-            val (from, to) = graph.getAdjacent(edge)
-            val ew = edge.weight
-            if (ew >= 0 && ew + from.weight >= 0 && ew + to.weight >= 0) {
-                merge(edge, from, to)
-            }
-        }
+        mergePositive()
+        mergeNegative()
+        negativeEdges()
+    }
+
+
+    private fun mergeNegative() {
         for (v in graph.vertexSet().toList()
                 .filter { it.weight <= 0 && graph.degreeOf(it) == 2 }) {
             val edges = graph.edgesOf(v).toTypedArray()
-            val nodes = edges.map { it -> graph.getOppositeVertex(v, it) }
+            if (edges[0].weight > 0 || edges[1].weight > 0)
+                continue
+            val nodes = edges.map { graph.getOppositeVertex(v, it) }
             val (l, r) = Pair(nodes[0], nodes[1])
-            graph.removeVertex(v);
+            graph.removeVertex(v)
             if (l != r) {
-                edges[0].absorb(v);
+                edges[0].absorb(v)
                 edges[0].absorb(edges[1])
                 graph.addEdge(l, r, edges[0])
             }
         }
     }
 
+    private fun mergePositive() {
+        for (edge in graph.edgeSet().toList()) {
+            if (!graph.containsEdge(edge))
+                continue
+            val (from, to) = graph.getAdjacent(edge)
+            val ew = edge.weight
+            if (ew >= 0 && ew + from.weight >= 0 && ew + to.weight >= 0) {
+                merge(edge, from, to)
+            }
+        }
+    }
+
+    private fun negativeEdges() {
+        graph.vertexSet()
+                .filter { n -> graph.edgesOf(n).any { it.weight <= 0 } }
+                .forEach {
+                    Dijkstra(graph, it).negativeEdges()
+                            .forEach { graph::removeEdge }
+                }
+    }
+
     private fun merge(e: Edge, l: Node, r: Node): Unit {
         if (!listOf(l, r).containsAll(graph.getAdjacent(e).toList()))
             throw IllegalArgumentException()
-        contract(e);
+        contract(e)
     }
 
     private fun contract(e: Edge): Unit {
